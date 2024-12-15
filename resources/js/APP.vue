6bc3492b5402e9a550581c07c5013ec86a86e794
vitou <pakvitou168@gmail.com>
2024-12-15 14:57:21 +0700

    init

---
+++ resources/js/APP.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Vue 3 + Laravel Application</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Welcome to your new application!'
+        }
+    }
+}
+</script>
